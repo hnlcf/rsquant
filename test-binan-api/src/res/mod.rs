d75045d67b90c3b963c7d50e8ab9ee9d4762c815
Changfeng <louchangfeng@outlook.com>
2023-05-08 23:05:05 +0800

    fix(res): move api to a single module

--- test-binan-api/src/res/mod.rs
+++ test-binan-api/src/res/mod.rs
@@ -1,33 +1,7 @@
-use hyper::client::HttpConnector;
-use hyper_tls::HttpsConnector;
-
-pub use account_info::AccountInfoRes;
-use binance_spot_connector_rust::http::request::Request;
-use binance_spot_connector_rust::http::Credentials;
-use binance_spot_connector_rust::hyper::BinanceHttpClient;
-use binance_spot_connector_rust::trade::account::Account;
-
 mod account_info;
+mod api;
+mod kline;
 
-type BinanHttpClient = BinanceHttpClient<HttpsConnector<HttpConnector>>;
-
-pub trait BinanResponse {}
-
-pub async fn get_account_info(
-    client: &BinanHttpClient,
-    credentials: &Credentials,
-) -> AccountInfoRes {
-    let request: Request = Account::default()
-        .credentials(credentials)
-        .recv_window(5000)
-        .into();
-
-    let data = client
-        .send(request)
-        .await
-        .expect("Failed to send request.")
-        .into_body_str()
-        .await
-        .expect("Failed to convert response body into string.");
-    serde_json::from_str(&data).expect("Failed to parse `AccountInfo`.")
-}
+pub use api::get_account_info;
+pub use api::get_kline;
+pub use api::BinanResponse;
