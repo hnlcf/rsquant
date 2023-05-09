9ef0d394a5e924667fbed9e61d97fc97e488d2d9
Changfeng <louchangfeng@outlook.com>
2023-05-09 22:29:18 +0800

    build(cargo): add sqlite as db

--- test-binan-api/src/bin/main.rs
+++ test-binan-api/src/bin/main.rs
@@ -1,15 +1,25 @@
+#![allow(dead_code)]
+use std::{thread, time::Duration};
+
 use chrono::Local;
 use fern::colors::{Color, ColoredLevelConfig};
 use hyper::client::HttpConnector;
 use hyper_tls::HttpsConnector;
 use lazy_static::lazy_static;
+use rusqlite::{Connection, Result};
 
 use binance_spot_connector_rust::{
     http::Credentials,
     hyper::{BinanceHttpClient, Error as BinanHyperError},
     market::klines::KlineInterval,
 };
-use test_binan_api::{credential, res, util};
+use test_binan_api::{
+    credential, res,
+    util::{
+        self,
+        time::{CurrentTime, TimeConverter},
+    },
+};
 
 type BinanHttpClient = BinanceHttpClient<HttpsConnector<HttpConnector>>;
 
@@ -38,7 +48,7 @@ fn setup_logger() -> Result<(), fern::InitError> {
                 message
             ))
         })
-        .level(log::LevelFilter::Trace)
+        .level(log::LevelFilter::Debug)
         .chain(std::io::stdout())
         .chain(fern::log_file(log_file)?)
         .apply()?;
@@ -49,19 +59,49 @@ fn setup_logger() -> Result<(), fern::InitError> {
 async fn main() -> Result<(), BinanHyperError> {
     setup_logger().expect("Can't setup logger");
 
-    let account_info = res::api::get_account_info(&CLIENT, &CREDENTIALS)
-        .await
-        .remove_blank_coin();
+    let conn = Connection::open("bitcoin.db").expect("Can't open database in sqlite");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS assets_price (
+            id          INTEGER PRIMARY KEY,
+            name        TEXT NOT NULL,
+            price       TEXT NOT NULL,
+            unix_time   INTEGER NOT NULL,
+            date_time   TEXT NOT NULL
+         )",
+        (),
+    )
+    .expect("Can't create table in sqlite");
+
+    let account_info = get_account_info().await;
     log::info!("Account info:\n{}", account_info);
 
-    let kline = get_kline(
-        "ETHUSDT",
-        KlineInterval::Hours1,
-        "2023-05-08 11:00:00",
-        "2023-05-09 11:00:00",
-    )
-    .await;
-    log::info!("{:#?}", kline);
+    let seconds = Duration::from_secs(2);
+    let mut count = 0;
+
+    loop {
+        if count > 10000 {
+            break;
+        }
+        count += 1;
+
+        thread::sleep(seconds);
+
+        let current_date_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        let current_unix_time = TimeConverter::date_to_unix_time(&current_date_time).unwrap_or(0);
+        let eth_price = get_ticker_price("ETHUSDT").await;
+        conn.execute(
+            "INSERT INTO assets_price (name, price, unix_time, date_time) VALUES (?1, ?2, ?3, ?4)",
+            (
+                &eth_price.symbol,
+                &eth_price.price,
+                &current_unix_time,
+                &current_date_time,
+            ),
+        )
+        .expect("Can't insert ticker price into sqlite");
+
+        log::info!("{:?}", eth_price);
+    }
 
     Ok(())
 }
@@ -69,6 +109,35 @@ async fn main() -> Result<(), BinanHyperError> {
 async fn get_ticker_price(symbol: &str) -> res::ticker_price::TickerPriceRes {
     res::api::get_ticker_price(&CLIENT, symbol).await
 }
+
+/// # Get account information
+///
+/// ## Examples
+///
+/// ```
+/// let account_info = get_account_info().await;
+/// println!("{:#?}", account_info);
+/// ```
+async fn get_account_info() -> res::account_info::AccountInfoRes {
+    res::api::get_account_info(&CLIENT, &CREDENTIALS)
+        .await
+        .remove_blank_coin()
+}
+
+/// # Get Kline data
+///
+/// ## Examples
+///
+/// ```
+/// let kline = get_kline(
+///     "ETHUSDT",
+///     KlineInterval::Hours1,
+///     "2023-05-08 11:00:00",
+///     "2023-05-09 11:00:00",
+/// )
+/// .await;
+/// println!("{:#?}", kline);
+/// ```
 async fn get_kline(
     symbol: &str,
     interval: KlineInterval,
