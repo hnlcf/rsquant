fcc33a344da45a93ba82ea8e791e1f752b43eb1b
Changfeng <louchangfeng@outlook.com>
2023-05-13 20:00:47 +0800

    fix(util): define logger

--- crates/test_binan_api/src/util/log.rs
+++ crates/test_binan_api/src/util/log.rs
@@ -5,28 +5,32 @@ use crate::util;
 
 const DEFAULT_LOG_FILE: &str = "log/output.log";
 
-pub fn setup_logger() -> Result<(), fern::InitError> {
-    let colors = ColoredLevelConfig::new()
-        .info(Color::Green)
-        .warn(Color::Yellow)
-        .debug(Color::White)
-        .error(Color::Red)
-        .trace(Color::Blue);
-    let log_file =
-        util::env::EnvManager::get_env_var("BINAN_LOG_FILE").unwrap_or(DEFAULT_LOG_FILE.into());
-    fern::Dispatch::new()
-        .format(move |out, message, record| {
-            out.finish(format_args!(
-                "[{} {} {}] {}",
-                Local::now().format("%Y-%m-%d %H:%M:%S"),
-                colors.color(record.level()),
-                record.target(),
-                message
-            ))
-        })
-        .level(log::LevelFilter::Debug)
-        .chain(std::io::stdout())
-        .chain(fern::log_file(log_file)?)
-        .apply()?;
-    Ok(())
+pub struct Logger(());
+
+impl Logger {
+    pub fn setup_logger() -> Result<(), fern::InitError> {
+        let colors = ColoredLevelConfig::new()
+            .info(Color::Green)
+            .warn(Color::Yellow)
+            .debug(Color::White)
+            .error(Color::Red)
+            .trace(Color::Blue);
+        let log_file =
+            util::env::EnvManager::get_env_var("BINAN_LOG_FILE").unwrap_or(DEFAULT_LOG_FILE.into());
+        fern::Dispatch::new()
+            .format(move |out, message, record| {
+                out.finish(format_args!(
+                    "[{} {} {}] {}",
+                    Local::now().format("%Y-%m-%d %H:%M:%S"),
+                    colors.color(record.level()),
+                    record.target(),
+                    message
+                ))
+            })
+            .level(log::LevelFilter::Debug)
+            .chain(std::io::stdout())
+            .chain(fern::log_file(log_file)?)
+            .apply()?;
+        Ok(())
+    }
 }
