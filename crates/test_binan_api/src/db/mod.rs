6cf20e2620d9380d0dcf9ab4c89a4d42ec5fa735
Changfeng <louchangfeng@outlook.com>
2023-05-13 20:01:45 +0800

    feat(db): create recorder to manager database for recording data

--- crates/test_binan_api/src/db/mod.rs
+++ crates/test_binan_api/src/db/mod.rs
@@ -1,1 +1,4 @@
+pub mod recorder;
 pub mod sqlite;
+
+pub trait DBConnection {}
--- crates/test_binan_api/src/db/sqlite.rs
+++ crates/test_binan_api/src/db/sqlite.rs
@@ -1,5 +1,7 @@
 use rusqlite::{Connection, Params};
 
+use super::DBConnection;
+
 pub struct SqliteConnection {
     conn: Option<Connection>,
 }
@@ -16,6 +18,24 @@ impl SqliteConnection {
         }
     }
 
+    pub fn insert_data<V: Params>(&self, table_name: &str, fields: &[&str], data: V) {
+        let mut fields_name = vec![];
+        let mut values_count = vec![];
+        for (i, &f) in fields.iter().enumerate() {
+            fields_name.push(f.to_owned());
+            values_count.push(format!("?{}", i + 1));
+        }
+
+        let fields_name_str = fields_name.join(",");
+        let values_count_str = values_count.join(",");
+        let insert_sql = format!(
+            "INSERT INTO {} ({}) VALUES ({})",
+            table_name, fields_name_str, values_count_str
+        );
+
+        self.execute(&insert_sql, data);
+    }
+
     pub fn execute<P: Params>(&self, sql: &str, params: P) {
         if let Some(ref conn) = self.conn {
             let res = conn.execute(sql, params);
@@ -23,5 +43,15 @@ impl SqliteConnection {
                 log::warn!("{}", e);
             }
         }
+    }
+}
+
+impl DBConnection for SqliteConnection {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sql_insert() {
+        assert_eq!(1 + 1, 2);
     }
 }
