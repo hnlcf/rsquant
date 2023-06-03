38b08d871c99388c71c307974181cb0e79fe7bb7
Changfeng <louchangfeng@outlook.com>
2023-06-03 22:48:22 +0800

    fix(api): resend request when first send fail

--- crates/api/src/res/api/handle_response.rs
+++ crates/api/src/res/api/handle_response.rs
@@ -1,23 +1,55 @@
+use crate::res::BinanHttpClient;
+
+use binan_spot::{http::request::Request, hyper::Response};
 use serde::Deserialize;
 
-use binan_spot::http::request::Request;
-
-use crate::res::BinanHttpClient;
+use std::process::abort;
 
 pub struct HandleResponse;
 
 impl HandleResponse {
     pub fn decode_response<'a, T: Deserialize<'a>>(data: &'a str) -> T {
-        serde_json::from_str(data).expect("Failed to parse response.")
+        match serde_json::from_str(data) {
+            Ok(t) => {
+                log::info!("Deserialize response string to data structure.");
+                t
+            }
+            Err(e) => {
+                log::error!(
+                    "Failed to deserialize response string to data structure: {}.",
+                    e
+                );
+                abort();
+            }
+        }
     }
 
     pub async fn get_response(client: &BinanHttpClient, request: impl Into<Request>) -> String {
-        client
-            .send(request)
-            .await
-            .expect("Failed to send request.")
-            .into_body_str()
-            .await
-            .expect("Failed to convert response body into string.")
+        let request = request.into();
+        loop {
+            match client.send(request.clone()).await {
+                Ok(res) => {
+                    log::info!("Send request from client.");
+                    return Self::response_to_string(res).await;
+                }
+                Err(e) => {
+                    log::error!("Failed to send request: {}. Resend it.", e);
+                    continue;
+                }
+            }
+        }
+    }
+
+    pub async fn response_to_string(res: Response) -> String {
+        match res.into_body_str().await {
+            Ok(s) => {
+                log::info!("Convert response into body string.");
+                s
+            }
+            Err(e) => {
+                log::error!("Failed to convert response body into string: {}.", e);
+                abort();
+            }
+        }
     }
 }
