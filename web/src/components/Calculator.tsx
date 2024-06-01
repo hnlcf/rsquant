cb9a7b32ab671cb870f672f88e7540dbc029363a
Changfeng Lou <louchangfeng@outlook.com>
2024-06-01 20:06:58 +0800

    fix(web): correct state of symbol and corresponding price to children components

--- web/src/components/Calculator.tsx
+++ web/src/components/Calculator.tsx
@@ -1,4 +1,4 @@
-import { Component, Show, createSignal } from "solid-js"
+import { Component, Show, createEffect, createSignal } from "solid-js"
 
 type CalculatorProps = {
   symbol: string
@@ -31,6 +31,8 @@ function compute_margin(
 }
 
 const Calculator: Component<CalculatorProps> = (props) => {
+  const [symbol, setSymbol] = createSignal<string>("BTCUSDT")
+  const [customPrice, setCustomPrice] = createSignal<boolean>(false)
   const [leverage, setLeverage] = createSignal<number>(10)
   const [price, setPrice] = createSignal<number>(100)
   const [side, setSide] = createSignal<"LONG" | "SHORT">("LONG")
@@ -39,128 +41,150 @@ const Calculator: Component<CalculatorProps> = (props) => {
 
   const [closePrice, setClosePrice] = createSignal<ClosePrice>()
 
-  const updateClosePrice = () => {
-    const close_price: ClosePrice = compute_margin(side(), price(), leverage(), stopLoss(), takeProfit())
+  const updateClosePrice = (price: number) => {
+    const close_price: ClosePrice = compute_margin(side(), price, leverage(), stopLoss(), takeProfit())
     setClosePrice(close_price)
   }
 
+  createEffect(() => {
+    if (!customPrice()) {
+      setSymbol(props.symbol)
+      setPrice(props.price ?? 0)
+      updateClosePrice(price())
+    }
+  })
+
   return (
-    <div class="flex flex-col">
-      <form class="flex flex-col p-4 shadow-md rounded-lg">
-        <div class="block text-center text-xl font-bold mb-8 text-gray-900 dark:text-white">CALCULATOR</div>
-        <div class="m-2">
-          <label for="leverage-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Leverage:
-          </label>
-          <div class="relative flex items-center max-w-[14rem]">
+    <div class="flex-col justify-around items-center">
+      <div class="p-10">
+        <form class="flex flex-col p-4 shadow-md rounded-lg">
+          <div class="block text-center text-xl font-bold mb-8 text-gray-900 dark:text-white">CALCULATOR</div>
+          <div class="m-2">
+            <label for="leverage-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Leverage (1x-125x):
+            </label>
+            <div class="relative flex items-center max-w-[14rem]">
+              <input
+                type="text"
+                id="leverage-input"
+                value={leverage()}
+                onInput={(e) => setLeverage(parseInt(e.currentTarget.value))}
+                data-input-counter
+                aria-describedby="helper-text-explanation"
+                class="rounded-lg bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="10X"
+                required
+              />
+            </div>
+          </div>
+
+          <div class="m-2 flex flex-col">
+            <label for="price-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Price ($):
+            </label>
+            <div class="relative flex items-center">
+              <input
+                type="text"
+                id="price-input"
+                value={price()}
+                onInput={(e) => setPrice(parseFloat(e.currentTarget.value))}
+                data-input-counter
+                aria-describedby="helper-text-explanation"
+                class="rounded-lg bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block mr-4 w-2/3 py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="67600"
+                required
+              />
+              <label class="inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  class="sr-only peer"
+                  onInput={(e) => {
+                    setSymbol("<default>")
+                    setCustomPrice(e.currentTarget.checked)
+                  }}
+                />
+                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Custom Price</span>
+              </label>
+            </div>
+          </div>
+
+          <div class="m-2 flex flex-col">
+            <label for="side-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Side:
+            </label>
+            <select
+              id="side-input"
+              onInput={(e) => setSide(e.currentTarget.value as "LONG" | "SHORT")}
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option>LONG</option>
+              <option>SHORT</option>
+            </select>
+          </div>
+
+          <div class="m-2 flex flex-col">
+            <label for="stop-loss-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Stop Loss (%):
+            </label>
             <input
               type="text"
-              id="leverage-input"
-              value={leverage()}
-              onInput={(e) => setLeverage(parseInt(e.currentTarget.value))}
+              id="stop-loss-input"
+              value={stopLoss()}
+              onInput={(e) => setStopLoss(parseInt(e.currentTarget.value))}
               data-input-counter
               aria-describedby="helper-text-explanation"
               class="rounded-lg bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="10X"
+              placeholder="10"
               required
             />
           </div>
-        </div>
 
-        <div class="m-2 flex flex-col">
-          <label for="price-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Price ($):
-          </label>
-          <div class="relative flex items-center max-w-[14rem]">
+          <div class="m-2 flex flex-col">
+            <label for="take-profit-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Take profit (%):
+            </label>
             <input
               type="text"
-              id="price-input"
-              value={price()}
-              onInput={(e) => setPrice(parseInt(e.currentTarget.value))}
+              id="take-profit-input"
+              value={takeProfit()}
+              onInput={(e) => setTakeProfit(parseInt(e.currentTarget.value))}
               data-input-counter
               aria-describedby="helper-text-explanation"
               class="rounded-lg bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="67600"
+              placeholder="30"
               required
             />
           </div>
-        </div>
 
-        <div class="m-2 flex flex-col">
-          <label for="side-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Side:
-          </label>
-          <select
-            id="side-input"
-            onInput={(e) => setSide(e.currentTarget.value as "LONG" | "SHORT")}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          >
-            <option>LONG</option>
-            <option>SHORT</option>
-          </select>
-        </div>
-
-        <div class="m-2 flex flex-col">
-          <label for="stop-loss-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Stop Loss (%):
-          </label>
-          <input
-            type="text"
-            id="stop-loss-input"
-            value={stopLoss()}
-            onInput={(e) => setStopLoss(parseInt(e.currentTarget.value))}
-            data-input-counter
-            aria-describedby="helper-text-explanation"
-            class="rounded-lg bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="10"
-            required
-          />
-        </div>
-
-        <div class="m-2 flex flex-col">
-          <label for="take-profit-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Take profit (%):
-          </label>
-          <input
-            type="text"
-            id="take-profit-input"
-            value={takeProfit()}
-            onInput={(e) => setTakeProfit(parseInt(e.currentTarget.value))}
-            data-input-counter
-            aria-describedby="helper-text-explanation"
-            class="rounded-lg bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="30"
-            required
-          />
-        </div>
-
-        <div class="m-2 flex flex-col">
-          <button
-            type="submit"
-            class="font-medium tracking-tight text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 mt-4 ml-2 p-2"
-            onClick={(e) => {
-              e.preventDefault()
-              updateClosePrice()
-            }}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-
-      <Show
-        when={closePrice() != undefined}
-        fallback={<div class="p-4 m-2 text-center text-xl font-bold">Loading...</div>}
-      >
-        <div class="flex flex-col p-4 m-4 shadow-md rounded-lg">
-          <p class="block mb-2 text-xl font-mono font-bold text-gray-900 dark:text-white">
-            Stop Loss Price ({stopLoss()}%): {closePrice()?.stop_loss_price}
-          </p>
-          <p class="block mb-2 text-xl font-mono font-bold text-gray-900 dark:text-white">
-            Take Profit Price ({takeProfit()}%): {closePrice()?.take_profit_price}
-          </p>
-        </div>
-      </Show>
+          <div class="m-2 flex flex-col">
+            <button
+              type="submit"
+              class="font-medium tracking-tight text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 mt-4 ml-2 p-2"
+              onClick={(e) => {
+                e.preventDefault()
+                updateClosePrice(price())
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div class="p-5">
+        <Show when={closePrice() != undefined} fallback={<></>}>
+          <div class="flex flex-col p-4 m-4 shadow-md rounded-lg">
+            <p class="block mb-2 text-xl font-mono font-bold text-gray-900 dark:text-white">Symbol: {symbol()}</p>
+            <p class="block mb-2 text-xl font-mono font-bold text-gray-900 dark:text-white">Price ($): {price()}</p>
+            <p class="block mb-2 text-xl font-mono font-bold text-gray-900 dark:text-white">
+              Stop Loss Price ({stopLoss()}%): {closePrice()?.stop_loss_price}
+            </p>
+            <p class="block mb-2 text-xl font-mono font-bold text-gray-900 dark:text-white">
+              Take Profit Price ({takeProfit()}%): {closePrice()?.take_profit_price}
+            </p>
+          </div>
+        </Show>
+      </div>
     </div>
   )
 }
--- web/src/pages/SubscribeTicker.tsx
+++ web/src/pages/SubscribeTicker.tsx
@@ -5,6 +5,7 @@ import SymbolCard from "../components/SymbolCard"
 import Calculator from "../components/Calculator"
 
 const SubscribeTicker: Component = () => {
+  const [selectSymbol, setSelectSymbol] = createSignal("")
   const [symbol, setSymbol] = createSignal("")
   const [symbolList, setSymbolList] = createSignal<string[]>([])
   const [tickerMap, setTickerMap] = createSignal<Map<string, number>>(new Map())
@@ -56,76 +57,79 @@ const SubscribeTicker: Component = () => {
   }
 
   return (
-    <div class="h-full flex flex-col">
-      {/* Top Side */}
-      <div class="flex-1 flex h-50 my-6">
-        <div class="flex w-full">
-          {/* Symbol Selection Form */}
-          <div class="w-1/2 flex flex-col items-center justify-center">
-            <div id="symbol-form">
-              <div class="flex-1 items-center justify-center p-4 shadow-md rounded-lg m-4">
-                <p class="mb-4 text-xl font-bold">SUBSCRIBE</p>
-                <form class="">
-                  <input
-                    type="text"
-                    class="h-11 rounded-lg text-center"
-                    placeholder="BTCUSDT"
-                    value={symbol()}
-                    onInput={(e) => setSymbol(e.currentTarget.value)}
-                  />
-                  <button
-                    type="submit"
-                    class="font-medium tracking-tight text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 ml-2 p-2"
-                    onClick={(e) => {
-                      e.preventDefault()
-                      subscribeTicker(symbol())
-                      setSymbol("")
-                    }}
-                  >
-                    Submit
-                  </button>
-                </form>
-              </div>
+    <div class="relative z-0 flex h-full w-full">
+      <div class="relative h-full w-2/3 flex flex-col items-center justify-center">
+        <div class="flex-1 h-32 flex my-6">
+          <div id="symbol-form">
+            <div class="flex-1 items-center justify-center p-4 shadow-md rounded-lg m-4">
+              <p class="mb-4 text-xl font-bold">SUBSCRIBE</p>
+              <form class="">
+                <input
+                  type="text"
+                  class="h-11 rounded-lg text-center"
+                  placeholder="BTCUSDT"
+                  value={symbol()}
+                  onInput={(e) => setSymbol(e.currentTarget.value)}
+                />
+                <button
+                  type="submit"
+                  class="font-medium tracking-tight text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 ml-2 p-2"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    subscribeTicker(symbol())
+                    setSymbol("")
+                  }}
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
 
-              <div class="flex-1 items-center justify-center p-4 shadow-md rounded-lg m-4">
-                <p class="mb-4 text-xl font-bold">UNSUBSCRIBE</p>
-                <form class="">
-                  <input
-                    type="text"
-                    class="h-11 rounded-lg text-center"
-                    placeholder="BTCUSDT"
-                    value={symbol()}
-                    onInput={(e) => setSymbol(e.currentTarget.value)}
-                  />
-                  <button
-                    type="submit"
-                    class="font-medium tracking-tight text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 ml-2 p-2"
-                    onClick={(e) => {
-                      e.preventDefault()
-                      unsubscribeTicker(symbol())
-                      setSymbol("")
-                    }}
-                  >
-                    Submit
-                  </button>
-                </form>
-              </div>
+            <div class="flex-1 items-center justify-center p-4 shadow-md rounded-lg m-4">
+              <p class="mb-4 text-xl font-bold">UNSUBSCRIBE</p>
+              <form class="">
+                <input
+                  type="text"
+                  class="h-11 rounded-lg text-center"
+                  placeholder="BTCUSDT"
+                  value={symbol()}
+                  onInput={(e) => setSymbol(e.currentTarget.value)}
+                />
+                <button
+                  type="submit"
+                  class="font-medium tracking-tight text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 ml-2 p-2"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    unsubscribeTicker(symbol())
+                    setSymbol("")
+                  }}
+                >
+                  Submit
+                </button>
+              </form>
             </div>
           </div>
-          {/* Calculator */}
-          <div class="w-1/2 flex items-center justify-center">
-            <Calculator symbol="BTCUSDT" price={67600} />
+        </div>
+        <div class="flex-1 flex justify-center">
+          <div id="symbol-cards" class="container mx-auto">
+            <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+              <For each={symbolList()}>
+                {(symbol) => (
+                  <SymbolCard
+                    onClick={() => setSelectSymbol(symbol)}
+                    symbol={symbol}
+                    price={tickerMap().get(symbol)}
+                    class="cursor-default"
+                  />
+                )}
+              </For>
+            </div>
           </div>
         </div>
       </div>
 
-      {/* Symbol Card */}
-      <div class="flex-1 h-5/6 flex items-center justify-center">
-        <div id="symbol-cards" class="container mx-auto">
-          <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-            <For each={symbolList()}>{(symbol) => <SymbolCard symbol={symbol} price={tickerMap().get(symbol)} />}</For>
-          </div>
-        </div>
+      <div class="relative h-full w-1/3 flex items-center justify-center">
+        <Calculator symbol={selectSymbol()} price={tickerMap().get(selectSymbol())} />
       </div>
     </div>
   )
